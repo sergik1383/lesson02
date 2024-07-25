b67b2c941ff346dd5fe1e1eb1a979e0bfbadfdd4
sergik1383 <thuglife82@mail.ru>
2024-07-25 15:31:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+number_of_homework_completed=12
+number_of_hours_spent=1.5
+course_name= "python"
+a=1.5
+b=12
+time_for_one_task=a/b
+print("курс:Pyton:","выполнил задач:",number_of_homework_completed,"затрачено часов:",number_of_hours_spent,"средние время выполненя:",time_for_one_task)
